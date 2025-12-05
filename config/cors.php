e01de5c5c54370ed4f6cb4ceb019bf6ccca11cd0
Mohamed122-r <anskajamal@gmail.com>
2025-12-05 07:50:06 +0200

    Create cors.php

---
+++ config/cors.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://mobile-store-frontend-pi.vercel.app',  // 🔄 رابط الفورنت الخاص بك
+        'https://*.vercel.app'
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
